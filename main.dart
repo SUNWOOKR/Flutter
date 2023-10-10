4385cdf89d78927dfe0defac1ada526d860f7918
선우 <ksw-12345@naver.com>
2023-10-10 19:47:54 +0900

    #1.5
    커밋

--- main.dart
+++ main.dart
@@ -1,3 +1,30 @@
 void main() {
-  final name = 'nico'; //final은 변경할 수 없다
+  /*
+  late final String name; //late는 변수를 나중에 할당할 수 있게 해준다.
+  //API로 테이터를 받아 변수에 넣어준다
+  name = 'nico';
+  print(name);
+  */
+
+  /*
+  dynamic name;
+  //name = 'nico';
+  //name = 12;
+  //name = true;
+  if (name != String){
+    name.블라
+  }
+  */
+
+  /*
+  String? sunwoo = 'nico'; //?를 넣으면 null일 수도 있고 아닐 수도 있다는걸 알려준다.
+  sunwoo = null;
+  if (sunwoo != null) {
+    sunwoo.isNotEmpty;
+  }
+  */
+
+  //nico?.isNotEmpty로 위에껄 줄일 수 있다.
+
+  //final name = 'nico'; //final은 변수를 한 번 할당한 후에 변경할 수 없다
 }
