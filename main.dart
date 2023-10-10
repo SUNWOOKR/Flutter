6356cd0d6b451cb6765d8ae9cb9cf91d2ad0dab6
선우 <ksw-12345@naver.com>
2023-10-10 19:38:08 +0900

    #1.4
    fianl은 변경할 수 없다

--- main.dart
+++ main.dart
@@ -1,8 +1,3 @@
 void main() {
-  String? sunwoo = 'nico'; //?를 넣으면 null일 수도 있고 아닐 수도 있다는걸 알려준다.
-  sunwoo = null;
-  if (sunwoo != null) {
-    sunwoo.isNotEmpty;
-  }
-  //nico?.isNotEmpty로 위에껄 줄일 수 있다.
+  final name = 'nico'; //final은 변경할 수 없다
 }
