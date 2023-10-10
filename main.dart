6c1a71ff51060df506a98e43e34434b433b02230
선우 <ksw-12345@naver.com>
2023-10-10 20:02:24 +0900

    #모르겄다

--- main.dart
+++ main.dart
@@ -1,4 +1,9 @@
 void main() {
+  //late var or late final or late String등을 써야한다.
+
+  //const name = 'nico';  //const도 fianl과 같다
+  //const는 앱을 컴파일하고 앱스토어에 올릴 때 그 값을 알고 있다면 const사용
+
   /*
   late final String name; //late는 변수를 나중에 할당할 수 있게 해준다.
   //API로 테이터를 받아 변수에 넣어준다
@@ -11,7 +16,7 @@ void main() {
   //name = 'nico';
   //name = 12;
   //name = true;
-  if (name != String){
+  if (name is String){
     name.블라
   }
   */
@@ -26,5 +31,5 @@ void main() {
 
   //nico?.isNotEmpty로 위에껄 줄일 수 있다.
 
-  //final name = 'nico'; //final은 변수를 한 번 할당한 후에 변경할 수 없다
+  //final String name = 'nico'; //final은 변수를 한 번 할당한 후에 변경할 수 없다
 }
