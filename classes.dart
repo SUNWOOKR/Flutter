0949915edad9e9e06d4c9320c7b41ae54c5504ec
선우 <ksw-12345@naver.com>
2023-10-14 11:52:25 +0900

    10/14
    10시 50분 다트 4.0

---
+++ classes.dart
@@ -0,0 +1,20 @@
+/*
+#4.0
+class Player {
+  final String name = 'nico';
+  int xp = 1500;
+
+  void sayHello() {
+    print("my name is $this.name");
+    //print("my name is $name");
+    //var의 이름들이 겹치면 this.name으로 쓰고 안 겹치면 그냥 name으로 써도 된다.
+  }
+}
+
+void main() {
+  var player = Player();
+  print(player.name);
+  player.sayHello();
+}
+*/
+void main() {}
