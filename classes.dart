6b8e80e2020acf5b835d3d5fb197f38e819aacb9
선우 <ksw-12345@naver.com>
2023-10-15 23:20:07 +0900

    10/15
    class recap까지

--- classes.dart
+++ classes.dart
@@ -16,5 +16,194 @@ void main() {
   print(player.name);
   player.sayHello();
 }
+#4.1
+class Player {
+  late final String name;
+  late int xp;
+
+  Player(String name, int xp) {
+    //constructors함수는 class와 이름이 같아야함
+    this.name = name;
+    this.xp = xp;
+  }
+
+  void sayHello() {
+    print("my name is $name");
+  }
+}
+
+void main() {
+  var player = Player("nico", 1500);
+  player.sayHello();
+  var player2 = Player("sunwoo", 3000);
+  player2.sayHello();
+
+  ==간략화==
+
+  class Player {
+  final String name;
+  int xp;
+
+  Player(this.name, this.xp);
+  //타입을 위에 이미 정해줬가 떄문에 this만 붙여도 가능하다.
+
+  void sayHello() {
+    print("my name is $name");
+  }
+}
+
+void main() {
+  var player = Player("nico", 1500);
+  player.sayHello();
+  var player2 = Player("sunwoo", 3000);
+  player2.sayHello();
+}
+}
+#4.2
+class Player {
+  final String name;
+  int xp;
+  String team;
+  int age;
+
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
+  //타입을 위에 이미 정해줬가 떄문에 this만 붙여도 가능하다.
+
+  void sayHello() {
+    print("my name is $name");
+  }
+}
+
+void main() {
+  var player = Player(
+    name: 'nico',
+    xp: 1200,
+    team: 'red',
+    age: 12,
+  );
+  player.sayHello();
+  var player2 = Player(
+    name: 'sunwoo',
+    xp: 3000,
+    team: 'red',
+    age: 12,
+  );
+  player2.sayHello();
+}
 */
-void main() {}
+
+// class Player {
+//   final String? name, team;
+//   int? xp, age;
+
+//   Player({this.name, this.xp, this.team, this.age});
+//   //타입을 위에 이미 정해줬가 떄문에 this만 붙여도 가능하다.
+
+//   void sayHello() {
+//     print("my name is $name");
+//   }
+// }
+
+// void main() {
+//   var player = Player(
+//     name: 'nico',
+//     xp: 1200,
+//     team: 'red',
+//     age: 12,
+//   );
+//   player.sayHello();
+//   var player2 = Player(
+//     name: 'sunwoo',
+//     xp: 3000,
+//     team: 'red',
+//     age: 12,
+//   );
+//   player2.sayHello();
+// }
+
+/*
+#4.3
+class Player {
+  final String name;
+  int xp, age;
+  String team;
+
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
+  //타입을 위에 이미 정해줬가 떄문에 this만 붙이고 타입을 안 붙여도 가능하다.
+
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        //:는 위에 있는 final String name,int xp,int age,Stromg name의 값을 초기화해준다.
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
+  void sayHello() {
+    print("my name is $name");
+  }
+}
+
+void main() {
+  var player = Player.createBluePlayer(
+    name: 'nico',
+    age: 12,
+  );
+  var redPlayer = Player.createRedPlayer('nico', 12);
+}
+*/
+
+class Player {
+  final String name;
+  int xp;
+  String team;
+
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void sayHello() {
+    print("my name is $name");
+  }
+}
+
+void main() {
+  var apiData = [
+    {
+      "name": "nico",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lynn",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "dal",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
+}
