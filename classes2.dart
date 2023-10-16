6eeefe4c89f69bbcc74415d1271eec68b31ab914
선우 <ksw-12345@naver.com>
2023-10-16 21:45:10 +0900

    finish dart basic
    다트 너무 어렵다.

--- classes2.dart
+++ classes2.dart
@@ -100,7 +100,6 @@ void main() {
 }
 
 #4.8
-*/
 
 class Human {
   final String name;
@@ -119,6 +118,12 @@ class Player extends Human {
     required this.team,
     required String name,
   }) : super(name: name); //super은 부모클래스와 상호작용하게 한다.
+
+  @override //Human에서 온 sayHello를 여기서 만든 sayHello로 대체한다.
+  void sayHello() {
+    super.sayHello();
+    print('and I play for ${team.name}');
+  }
 }
 
 void main() {
@@ -127,4 +132,43 @@ void main() {
     name: 'nico',
   );
   player.sayHello();
+}
+
+#4.9
+*/
+
+class Strong {
+  //Mixin의 조건은 생성자가 없는 클래스여야 한다는것!!!!!!
+  final double strengthLevel = 1500.99;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print('runnnnn');
+  }
+}
+
+class Tall {
+  final double hetght = 1.99;
+}
+
+enum Team { blue, red }
+
+class Player with Strong, QuickRunner, Tall {
+  final Team team;
+
+  Player({
+    required this.team,
+  }); //super은 부모클래스와 상호작용하게 한다.
+}
+
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
+void main() {
+  var player = Player(
+    team: Team.red,
+  );
+  player.runQuick();
 }
